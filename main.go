8a657bce0035ef04ae2a77a6cbd691160d1ef34a
Sean Callaway <Seancallaway@gmail.com>
2023-10-11 10:13:55 -0700

    Add code for Part 2 (#2)
    * Add Entity struct.
    
    * Add player to game.
    
    * Add player asset.
    
    * Refactor entities.
    
    * Fix player asset.
    
    * Render entities.
    
    * Add pointer to player in Game.
    
    * Add input handling for movement.

--- main.go
+++ main.go
@@ -7,23 +7,36 @@ import (
 )
 
 type Game struct {
-	Levels []Level
+	Levels       []Level
+	CurrentLevel *Level
+	Entities     []Entity
+	Player       *Entity
 }
 
 // Creates a new Game object and initializes the data.
 func NewGame() *Game {
 	g := &Game{}
 	g.Levels = append(g.Levels, NewLevel())
+	g.CurrentLevel = &g.Levels[0]
+
+	player, err := NewEntity(40, 25, "player")
+	if err != nil {
+		log.Fatal(err)
+	}
+	g.Entities = append(g.Entities, player)
+	g.Player = &g.Entities[0]
 	return g
 }
 
 func (g *Game) Update() error {
+	HandleInput(g)
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	level := g.Levels[0]
 	level.Draw(screen)
+	RenderEntities(g, level, screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
